b28e3a43b9b157de083b02ee514e2a0373c1acea
miyo-9 <jaejae960325@gmail.com>
2025-11-27 17:29:13 +0900

    Update ProductService.js

--- ProductService.js
+++ ProductService.js
@@ -5,13 +5,20 @@ export class ProductService {
     this.products = [];
   }
 
-  addProduct(product) {
-    if (product instanceof Product) {
-      this.products.push(product);
-    }
+  createProduct(name, description, price, tags = [], images = []) {
+    const product = new Product(name, description, price, tags, images);
+    this.products.push(product);
+    return product;
   }
 
   getProducts() {
     return this.products;
+  }
+
+  favoriteProduct(index) {
+    const product = this.products[index] ?? null;
+    if (!product) return null;
+    product.favorite();
+    return product;
   }
 }
