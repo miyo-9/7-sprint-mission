c6e12d86d82d40a1022269b856bd37c76a0cb1e0
miyo-9 <jaejae960325@gmail.com>
2025-11-27 17:25:27 +0900

    Update ArticleService.js

--- ArticleService.js
+++ ArticleService.js
@@ -5,13 +5,24 @@ export class ArticleService {
     this.articles = [];
   }
 
-  addArticle(article) {
-    if (article instanceof Article) {
-      this.articles.push(article);
-    }
+  createArticle(title, content, tags = [], images = []) {
+    const article = new Article(title, content, tags, images);
+    this.articles.push(article);
+    return article;
   }
 
   getArticles() {
     return this.articles;
+  }
+
+  getArticleByIndex(index) {
+    return this.articles[index] ?? null;
+  }
+
+  likeArticle(index) {
+    const article = this.getArticleByIndex(index);
+    if (!article) return null;
+    article.like();
+    return article;
   }
 }
