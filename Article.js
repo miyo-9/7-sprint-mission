9b5397265a47576306c595545bb31319e10b185c
miyo-9 <jaejae960325@gmail.com>
2025-11-27 17:24:42 +0900

    Update Article.js

--- Article.js
+++ Article.js
@@ -11,10 +11,3 @@ export class Article {
     this.likeCount += 1;
   }
 }
-
-export class NewsArticle extends Article {
-  constructor(title, content, tags = [], images = [], press) {
-    super(title, content, tags, images);
-    this.press = press;
-  }
-}
